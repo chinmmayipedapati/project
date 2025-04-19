4a6c8beb718f79a65f2f6137ad7bfbe9cd03bb63
see_bee <chinmmayipedapati@gmail.com>
2025-04-19 08:28:26 +0530

    main

--- app/routes.ts
+++ app/routes.ts
@@ -1,11 +1,13 @@
-export interface Route {
+interface Route {
   path: string;
   file: string;
 }
 
-export const routes: Route[] = [
+const routes: Route[] = [
   { path: "/", file: "routes/home.tsx" },
   { path: "about", file: "routes/about.tsx" },
   { path: "team", file: "routes/team.tsx" },
   { path: "service", file: "routes/service.tsx" }
 ];
+
+export default routes;
