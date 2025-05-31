c2eb0c056d2571042546503f2db4ba2140cc061a
see_bee <chinmmayipedapati@gmail.com>
2025-05-31 13:08:11 +0530

    main

--- app/welcome/about.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import ReactDOM from 'react-dom';
-import App from './App';
-import { motion } from 'framer-motion';
-import { Link } from 'react-router';
-
-ReactDOM.render(<App />, document.getElementById('root'));
